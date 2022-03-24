bf27eccf547b4d82ab78647a818b4b1ae9a85084
Krystian Piękoś <krystian.piekos@infotraining.pl>
2022-03-24 14:25:19 +0100

    Adapter - exercise solved

--- Structural/Adapter.Exercise/shape_readers_writers/text_reader_writer.cpp
+++ Structural/Adapter.Exercise/shape_readers_writers/text_reader_writer.cpp
@@ -7,11 +7,27 @@ using namespace Drawing;
 using namespace IO;
 
 // TODO - register creator for a TextReaderWriter class
+namespace
+{
+    bool is_registered = SingletonShapeRWFactory::instance()
+        .register_creator(make_type_index<Text>(), std::make_unique<TextReaderWriter>);
+}
 
 void TextReaderWriter::read(Shape& shp, istream& in)
 {
+    Text& text = static_cast<Text&>(shp);
+
+    Point pt;
+    std::string str;
+
+    in >> pt >> str;
+
+    text.set_coord(pt);
+    text.set_text(str);
 }
 
 void TextReaderWriter::write(const Shape& shp, ostream& out)
 {
+    const Text& text = static_cast<const Text&>(shp);
+    out << Text::id << " " << text.coord() << " " << text.text() << "\n";
 }
